9d77b9075feeda936eb065452211152754d94680
Dan Barber <github@danbarber.me>
2016-07-29 15:41:25 -0500

    Add categories plugin

---
+++ _plugins/categories.rb
@@ -0,0 +1,15 @@
+module Jekyll
+  module Filters
+
+    # Outputs a list of categories as comma-separated <a> links. This is used
+    # to output the category list for each post on a category page.
+    #
+    #  +categories+ is the list of categories to format.
+    #
+    # Returns string
+    #
+    def category_list(categories)
+      categories.sort.join(', ')
+    end
+  end
+end
