99607c37db3c685cb4a96e6cef5ca8d32df16b00
Dan Barber <github@danbarber.me>
2016-07-29 15:44:07 -0500

    Fix some hound complaints

--- _plugins/categories.rb
+++ _plugins/categories.rb
@@ -1,6 +1,5 @@
 module Jekyll
   module Filters
-
     # Outputs a list of categories as comma-separated <a> links. This is used
     # to output the category list for each post on a category page.
     #
@@ -9,7 +8,7 @@ module Jekyll
     # Returns string
     #
     def category_list(categories)
-      categories.sort.join(', ')
+      categories.sort.join(", ")
     end
   end
 end
