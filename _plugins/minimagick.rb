8b02ea04285fa32d477a94abf0807ac62e2c4d1b
Daniel Barber <github@danbarber.me>
2019-07-13 16:36:39 -0400

    Fix up feeds and OG data

---
+++ _plugins/minimagick.rb
@@ -0,0 +1,39 @@
+module Jekyll
+  module MiniMagick
+    def magick(image_path, options)
+      filename, ext = image_path.split("/").last.split(".")
+
+      dest_path = image_path.
+        split("/")[0..-2].
+        prepend("assets").
+        join("/")
+
+      options.each do |command, arg|
+        filename += "_#{command}#{arg}"
+      end
+
+      write_path = "#{dest_path}/#{filename}.#{ext}"
+
+      if File.exists?("_site/#{write_path}")
+        return write_path
+      end
+
+      image = ::MiniMagick::Image.open("_pictures/#{image_path}")
+
+      image.combine_options do |i|
+        options.each do |command, arg|
+          i.send command, arg
+        end
+        i.strip
+      end
+
+      puts write_path
+
+      image.write "_site/#{write_path}"
+
+      write_path
+    end
+  end
+end
+
+Liquid::Template.register_filter(Jekyll::MiniMagick)
